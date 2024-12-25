d53d10b774c624091480cd8146d48f7fc4377b32
Fijxu <fijxu@nadeko.net>
2024-12-25 08:55:41 -0300

    add health endpoint for healthchecks (#27)

---
+++ src/routes/health.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+
+const health = new Hono();
+
+health.get("/", () => {
+  return new Response("OK", {
+	status: 200,
+    headers: { "Content-Type": "text/plain" },
+  });
+});
+
+export default health;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ import youtubeApiPlayer from "./youtube_api_routes/player.ts";
 import invidiousRouteLatestVersion from "./invidious_routes/latestVersion.ts";
 import invidiousRouteDashManifest from "./invidious_routes/dashManifest.ts";
 import videoPlaybackProxy from "./videoPlaybackProxy.ts";
+import health from "./health.ts";
 
 export const routes = (app: Hono, konfigStore: Store<Record<string, unknown>>) => {
   app.use("*", logger());
@@ -22,4 +23,5 @@ export const routes = (app: Hono, konfigStore: Store<Record<string, unknown>>) =
   app.route("/latest_version", invidiousRouteLatestVersion);
   app.route("/api/manifest/dash/id", invidiousRouteDashManifest);
   app.route("/videoplayback", videoPlaybackProxy);
+  app.route("/healthz", health);
 };
