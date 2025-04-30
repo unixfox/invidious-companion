16d06cd856afa76a7fbef32b1b6cfdee0fd3912a
Émilien (perso) <4016501+unixfox@users.noreply.github.com>
2025-04-30 03:00:01 +0200

    fallback to other youtube clients in case of no adaptive formats (dash) (#119)
    * fallback to other youtube clients in case of no adaptive formats (dash)
    
    * better comment

--- src/lib/helpers/youtubePlayerReq.ts
+++ src/lib/helpers/youtubePlayerReq.ts
@@ -4,6 +4,35 @@ import type { TokenMinter } from "../jobs/potoken.ts";
 
 import type { Config } from "./config.ts";
 
+function callWatchEndpoint(
+    videoId: string,
+    innertubeClient: Innertube,
+    innertubeClientType: string,
+    contentPoToken: string,
+) {
+    const watch_endpoint = new NavigationEndpoint({
+        watchEndpoint: { videoId: videoId },
+    });
+
+    return watch_endpoint.call(
+        innertubeClient.actions,
+        {
+            playbackContext: {
+                contentPlaybackContext: {
+                    vis: 0,
+                    splay: false,
+                    lactMilliseconds: "-1",
+                    signatureTimestamp: innertubeClient.session.player?.sts,
+                },
+            },
+            serviceIntegrityDimensions: {
+                poToken: contentPoToken,
+            },
+            client: innertubeClientType,
+        },
+    );
+}
+
 export const youtubePlayerReq = async (
     innertubeClient: Innertube,
     videoId: string,
@@ -17,24 +46,50 @@ export const youtubePlayerReq = async (
         innertubeClientUsed = "TV";
     }
 
-    const watch_endpoint = new NavigationEndpoint({
-        watchEndpoint: { videoId: videoId },
-    });
-
     const contentPoToken = await tokenMinter(videoId);
 
-    return watch_endpoint.call(innertubeClient.actions, {
-        playbackContext: {
-            contentPlaybackContext: {
-                vis: 0,
-                splay: false,
-                lactMilliseconds: "-1",
-                signatureTimestamp: innertubeClient.session.player?.sts,
-            },
-        },
-        serviceIntegrityDimensions: {
-            poToken: contentPoToken,
-        },
-        client: innertubeClientUsed,
-    });
+    const youtubePlayerResponse = await callWatchEndpoint(
+        videoId,
+        innertubeClient,
+        innertubeClientUsed,
+        contentPoToken,
+    );
+
+    // Check if the first adaptive format URL is undefined, if it is then fallback to multiple YT clients
+
+    if (
+        !innertubeClientOauthEnabled &&
+        youtubePlayerResponse.data.streamingData &&
+        youtubePlayerResponse.data.streamingData.adaptiveFormats[0].url ===
+            undefined
+    ) {
+        console.log(
+            "[WARNING] No URLs found for adaptive formats. Falling back to other YT clients.",
+        );
+        const innertubeClientsTypeFallback = ["MWEB", "TV"];
+
+        for await (const innertubeClientType of innertubeClientsTypeFallback) {
+            console.log(
+                `[WARNING] Trying fallback YT client ${innertubeClientType}`,
+            );
+            const youtubePlayerResponseFallback = await callWatchEndpoint(
+                videoId,
+                innertubeClient,
+                innertubeClientType,
+                contentPoToken,
+            );
+            if (
+                youtubePlayerResponseFallback.data.streamingData &&
+                youtubePlayerResponseFallback.data.streamingData
+                    .adaptiveFormats[0].url
+            ) {
+                youtubePlayerResponse.data.streamingData.adaptiveFormats =
+                    youtubePlayerResponseFallback.data.streamingData
+                        .adaptiveFormats;
+                break;
+            }
+        }
+    }
+
+    return youtubePlayerResponse;
 };
